368a3add8ef4ca67ce5203e273cf8ceb371ccc38
John Scancella <blacksmithforlife@gmail.com>
2018-10-02 10:31:03 -0400

    adding BagReader tests

--- src/main/java/com/github/jscancella/reader/BagReader.java
+++ src/main/java/com/github/jscancella/reader/BagReader.java
@@ -1,13 +1,42 @@
 package com.github.jscancella.reader;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.AbstractMap.SimpleImmutableEntry;
 
 import com.github.jscancella.domain.Bag;
+import com.github.jscancella.domain.Version;
+import com.github.jscancella.exceptions.InvalidBagMetadataException;
+import com.github.jscancella.exceptions.InvalidBagitFileFormatException;
+import com.github.jscancella.exceptions.MaliciousPathException;
+import com.github.jscancella.exceptions.UnparsableVersionException;
+import com.github.jscancella.reader.internal.BagitTextFileReader;
+import com.github.jscancella.reader.internal.FetchReader;
+import com.github.jscancella.reader.internal.ManifestReader;
+import com.github.jscancella.reader.internal.MetadataReader;
 
 public enum BagReader {; //using enum to ensure singleton
-  public static Bag read(Path bagDirectory) {
+  public static Bag read(Path bagDirectory) 
+      throws InvalidBagMetadataException, IOException, UnparsableVersionException, InvalidBagitFileFormatException, MaliciousPathException {
     
-    return new Bag();
+    final Bag bag = new Bag();
+    final Path bagitFile = bagDirectory.resolve("bagit.txt");
+    final SimpleImmutableEntry<Version, Charset> bagitInfo = BagitTextFileReader.readBagitTextFile(bagitFile);
+    bag.setVersion(bagitInfo.getKey());
+    bag.setFileEncoding(bagitInfo.getValue());
+    bag.setRootDir(bagDirectory);
     
+    ManifestReader.readAllManifests(bagDirectory, bag);
+    
+    bag.getMetadata().addAll(MetadataReader.readBagMetadata(bagDirectory, bag.getFileEncoding()));
+    
+    final Path fetchFile = bagDirectory.resolve("fetch.txt");
+    if(Files.exists(fetchFile)){
+      bag.getItemsToFetch().addAll(FetchReader.readFetch(fetchFile, bag.getFileEncoding(), bag.getRootDir()));
+    }
+    
+    return bag;
   }
 }
